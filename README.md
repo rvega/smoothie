0acf8c16ebd170d24c3eac8607f10e00ebe86e32
letorbi <public@letorbi.de>
2013-03-17 18:00:43 +0100

    Jay renamed to Smoothie

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-Jay - Yet another JavaScript library - but different :)
+Smoothie - a lightweight and modular JavaScript library
 =======================================================
 
 Well, I guess it will still take some time until this repository here contains
-a whole library ;)
+a whole library, but I'm working on it ;)
 
 Right now it only contains my implementation of the CommonJS module
 specification. More information can be found in my post at Pixel vs. Bytes:
 http://pixelsvsbytes.com/blog/2013/02/js-require-for-browsers-better-faster-stronger/
 
-Jay is licensed as open source under the LGPL v3.
+Smoothie is licensed as open source under the LGPL v3.
--- require.js
+++ require.js
@@ -1,16 +1,16 @@
 //
-// This file is part of Jay.
+// This file is part of Smoothie.
 //
-// Jay is free software: you can redistribute it and/or modify it under the
+// Smoothie is free software: you can redistribute it and/or modify it under the
 // terms of the GNU Lesser General Public License as published by the Free
 // Software Foundation, either version 3 of the License, or (at your option) any
 // later version.
 //
-// Jay is distributed in the hope that it will be useful, but WITHOUT ANY
+// Smoothie is distributed in the hope that it will be useful, but WITHOUT ANY
 // WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 // A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
 // details.You should have received a copy of the GNU Lesser General Public
-// License along with Jay.  If not, see <http://www.gnu.org/licenses/>.
+// License along with Smoothie.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 // NOTE The load parameter points to the function, which prepares the
